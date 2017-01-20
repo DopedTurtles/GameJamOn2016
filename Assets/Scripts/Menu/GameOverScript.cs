f450cfb47170437d8b54ad95accf9e8fe141b50b
acburgos <alberto.casb@gmail.com>
2017-01-21 00:52:14 +0100

    Subiendo escenas y demases

---
+++ Assets/Scripts/Menu/GameOverScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameOverScript : MonoBehaviour {
+
+    //Variables del propio Menu
+    private CGameController gameController;
+
+    private void Start()
+    {
+        gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<CGameController>();
+    }
+
+    /*
+     * StartPulsado: Funcion llamada en el menu principal cuando se pulsa el boton Start
+     *   - Carga la escena que contiene el juego
+     */
+    public void RetryPulsado()
+    {
+        //Aqui fundido a negro
+        gameController.Retry();
+    }
+
+    /*
+     * QuitPulsado: Funcion llamada en el menu principal cuando se pulsa el boton Quit
+     *   - Cierra el juego
+     */
+    public void ToMenuPulsado()
+    {
+        gameController.CargaNivel("MainMenu");
+    }
+}
---
+++ Assets/Scripts/Menu/MainMenuScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuScript : MonoBehaviour {
+
+    //Variables del propio Menu
+    private CGameController gameController;
+
+    private void Start()
+    { 
+        gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<CGameController>();
+    }
+
+    /*
+     * StartPulsado: Funcion llamada en el menu principal cuando se pulsa el boton Start
+     *   - Carga la escena que contiene el juego
+     */
+    public void StartPulsado()
+    {
+        //Aqui fundido a negro
+        gameController.CargaNivel("Level1");
+    }
+
+    /*
+     * QuitPulsado: Funcion llamada en el menu principal cuando se pulsa el boton Quit
+     *   - Cierra el juego
+     */
+    public void QuitPulsado()
+    {
+        Application.Quit();
+    }
+}
