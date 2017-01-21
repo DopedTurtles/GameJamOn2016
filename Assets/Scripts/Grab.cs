66bd73afe86380f63888bc698801b6cb3e203d61
Razvan <rzn.arato@gmail.com>
2017-01-21 01:35:45 +0100

    Grab and Move_v2

---
+++ Assets/Scripts/Grab.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grab : MonoBehaviour {
+    private float positionShip;
+    private float   offSet;
+    private float originalFall;
+	// Use this for initialization
+	void Start () {
+        positionShip = transform.parent.position.z;
+        offSet       = 0.5f;
+        originalFall = gameObject.GetComponent<Move>().forceFall;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        positionShip = transform.parent.position.z;
+        grabCenterShip();
+        Debug.Log(positionShip);
+    }
+
+    void grabCenterShip()
+    {
+        if(offSet + positionShip > transform.position.z && positionShip - offSet < transform.position.z) gameObject.GetComponent<Move>().forceFall = 0;
+        else gameObject.GetComponent<Move>().forceFall = originalFall;
+    }
+}
--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -3,19 +3,41 @@ using System.Collections;
 
 public class Move : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    private float rotationShip;
+    public  float forceAcceleration;
+    public  float forceFall;
+    // Use this for initialization
+    void Start () {
+        rotationShip = transform.parent.rotation.x;
+        Debug.Log(rotationShip);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        rotationShip = transform.parent.rotation.x;
+        movePlayerInclination();
+    }
 
     public void movePlayer(bool moveLeft, float speed)
     {
-        if (moveLeft) transform.Translate(-Vector3.forward * Time.deltaTime * speed);
-        else transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        
+        if (moveLeft)
+        {
+           if (rotationShip > 0) speed -= forceAcceleration * rotationShip;
+           else speed -= forceAcceleration * rotationShip;
+           transform.Translate(-Vector3.forward * Time.deltaTime * speed);
+        }
+        else
+        {
+            if (rotationShip > 0) speed += forceAcceleration * rotationShip;
+            else speed += forceAcceleration * rotationShip;
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+    }
+
+    public void movePlayerInclination()
+    {
+        if (rotationShip > 0) transform.Translate(Vector3.forward * Time.deltaTime * rotationShip * forceFall);
+        else transform.Translate(Vector3.forward * Time.deltaTime * rotationShip * forceFall);
     }
 }
