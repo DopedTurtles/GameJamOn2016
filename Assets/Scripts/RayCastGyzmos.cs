05c71607cbeab5a341d742b7b928834ff49116a2
acburgos <alberto.casb@gmail.com>
2017-01-21 03:54:13 +0100

    arreglando barco de mario

---
+++ Assets/Scripts/RayCastGyzmos.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RayCastGyzmos : MonoBehaviour {
+
+    public float radioEsfera=1f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(transform.position, radioEsfera);
+    }
+}
