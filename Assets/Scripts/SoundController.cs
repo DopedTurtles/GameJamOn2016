f5883c226dc14dc7285b55f756db549b3a27faf2
acburgos <alberto.casb@gmail.com>
2017-01-21 01:52:05 +0100

    borrando metas

---
+++ Assets/Scripts/SoundController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using System.Collections;
+
+public class SoundController : MonoBehaviour {
+
+    //Variables propias de SoundController
+    public AudioSource backgroundMusic;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+}
