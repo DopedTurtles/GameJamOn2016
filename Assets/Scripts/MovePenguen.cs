9d4ed8f683b1a40c9840d6bc681c3df22577869e
Razvan <rzn.arato@gmail.com>
2017-01-21 15:04:35 +0100

    Move Penguin And Seagull

---
+++ Assets/Scripts/MovePenguen.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePenguen : MonoBehaviour
+{
+    public float RSpeed = 25;
+    public float speed = 0.01f;
+    private float pesoOriginal;
+    GameObject ship;
+    void Start()
+    {
+        ship = GameObject.FindGameObjectWithTag("Barco");
+        pesoOriginal = this.GetComponent<CPesoPlayer>().mPeso;
+        this.GetComponent<CPesoPlayer>().mPeso = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.parent.Rotate(Vector3.right * Time.deltaTime * RSpeed, Space.World);
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Barco")
+        {
+            RSpeed = 0;
+            StartCoroutine("Desplazar");
+           // StartCoroutine("Levantar");
+        }
+
+    }
+  
+    IEnumerator Desplazar()
+    {
+        for (int f = 0; f < 20; f++)
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * 10f);
+            yield return null;
+        }
+        StartCoroutine("Levantar");
+    }
+    IEnumerator Levantar()
+    {
+        for (int f = 0; f < 50; f++)
+        {
+            transform.Rotate(-Vector3.right * Time.deltaTime * 120, Space.World);
+            yield return null;
+        }
+        this.GetComponent<CPesoPlayer>().mPeso = pesoOriginal;
+    }
+}
---
+++ Assets/Scripts/MoveSeagull.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveSeagull : MonoBehaviour {
+    public Transform siteToGo;
+    private float distance ;
+    public float speedSeagull = 10;
+    private float pesoOriginal;
+    // Use this for initialization
+    void Start () {
+        pesoOriginal = this.GetComponent<CPesoPlayer>().mPeso;
+        this.GetComponent<CPesoPlayer>().mPeso = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(siteToGo.position);
+        distance = Vector3.Distance(transform.position, siteToGo.position);
+        if (distance >= 0.5)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speedSeagull);
+
+        }else this.GetComponent<CPesoPlayer>().mPeso = pesoOriginal;
+
+    }
+  
+}
