1284adc97f1ce6d18fb979418f15a225bbab8adb
Razvan <rzn.arato@gmail.com>
2017-01-22 02:34:11 +0100

    Merge Definitivo

--- Assets/Scripts/CBarcoController.cs
+++ Assets/Scripts/CBarcoController.cs
@@ -9,29 +9,44 @@ public class CBarcoController : MonoBehaviour
 
     List<float> paraPesar;
 
-    public float mRotationSpeed = 0f;
+    private float mRotationSpeed = 0f;
+	private float mTotalRotation = 0f;
     
     public string mTagPlayer1 = "Player1";
     public string mTagPlayer2 = "Player2";
     public Transform boatFront;
     public Transform boatBack;
+	public Transform boatCenter;
 
 
-    public float y2 = 0;
+    //public float y2 = 0;
     public float rotationSpeedWaves = 1f;
     public float rotationSpeedPlayer = 1f;
+	public float rotationEnemyCoefficient = 1f;
+	public float rotationSpeedFriction = 1f;
     //Variables para RayTrace
     public float maxDistancia;
     public float rayDuration = 3f;
     private float frontDistance;
     private float backDistance;
-    public float offsetY = 0;
+    private float offsetY = 0;
+
+	private bool mEndGame = false;
+	public float mEndAnimationSpeed = 1.0f;
+	public float mEndDelay = 3.0f;
 
     // Use this for initialization
     void Start()
     {
         paraPesar = new List<float>();
     }
+
+	void OnDrawGizmos()
+	{
+		Gizmos.DrawSphere (boatFront.position, 0.1f);
+		Gizmos.DrawSphere (boatBack.position, 0.1f);
+		Gizmos.DrawSphere (boatCenter.position, 0.1f);
+	}
 
     // Update is called once per frame
     void Update()
@@ -40,21 +55,27 @@ public class CBarcoController : MonoBehaviour
         RaycastHit backHit;
         RaycastHit middleHit;
 
-        Debug.DrawRay(boatFront.position, Vector3.down, Color.red);
-        Debug.DrawRay(boatBack.position, Vector3.down, Color.red);
-        if (Physics.Raycast(boatFront.position, Vector3.down, out frontHit, maxDistancia))
-            frontDistance = frontHit.distance;
+		//Debug.DrawRay(boatFront.position, Vector3.down, Color.red);
+		//Debug.DrawRay(boatBack.position, Vector3.down, Color.red);
+		Debug.DrawRay(boatCenter.position, Vector3.down, Color.red);
+
+		if (Physics.Raycast (boatFront.position, Vector3.down, out frontHit, maxDistancia)) {
+			frontDistance = frontHit.distance;
+			Debug.DrawRay (boatFront.position, Vector3.down * frontDistance);
+		}
         else
             frontDistance = 0;
 
-        if (Physics.Raycast(boatBack.position, Vector3.down, out backHit, maxDistancia))
-            backDistance = backHit.distance;
+		if (Physics.Raycast (boatBack.position, Vector3.down, out backHit, maxDistancia)) {
+			backDistance = backHit.distance;
+			//Debug.DrawRay (boatBack.position, Vector3.down * backDistance);
+		}
         else
             backDistance = 0;
 
-        if (Physics.Raycast(transform.position, Vector3.down, out middleHit, maxDistancia))
+		if (Physics.Raycast(boatCenter.position, Vector3.down, out middleHit, maxDistancia))
         {
-            y2 = middleHit.point.y;
+            //y2 = middleHit.point.y;
             offsetY = middleHit.point.y;
         }
 
@@ -63,7 +84,13 @@ public class CBarcoController : MonoBehaviour
 
     void LateUpdate()
     {
-        this.CalculateRotation();
+		if (!mEndGame) {
+			CalculateHeight ();
+			this.CalculateRotation ();
+			checkRotation ();
+		} else {
+			EndGameAnimation ();
+		}
     }
 
     /**
@@ -72,7 +99,17 @@ public class CBarcoController : MonoBehaviour
     private void RotateShip(float rotation)
     {
         this.transform.Rotate(rotation * Time.deltaTime, 0, 0);
+		mTotalRotation += rotation * Time.deltaTime;
     }
+
+	void CalculateHeight()
+	{
+		this.transform.position = new Vector3(transform.position.x, offsetY + 1.5f, transform.position.z);
+	}
+	void EndGameAnimation()
+	{
+		this.transform.position += Vector3.down * Time.deltaTime * mEndAnimationSpeed;
+	}
 
     private void CalculateRotation()
     {
@@ -90,31 +127,44 @@ public class CBarcoController : MonoBehaviour
     private void CalculateRotationWave(float aceleracionPlayers)
     {
         float pendiente1 = 0;
-        if (backDistance>0 && frontDistance>0)
+        //if (backDistance>0 && frontDistance>0)
         pendiente1 = Mathf.Atan((backDistance - frontDistance) / (Mathf.Abs(boatBack.position.z - boatFront.position.z)));
-        if (backDistance <= 0) { 
+        /*if (backDistance <= 0) { 
             pendiente1 -= 10 * frontDistance;
             }
         if (frontDistance <= 0)
         {
                 pendiente1 += 10 * backDistance;
-        }
+        }*/
         float pendiente2 = this.transform.rotation.x;
 
-        aceleracion = (-(pendiente2 - pendiente1) * rotationSpeedWaves) + (aceleracionPlayers * rotationSpeedPlayer);
+		//Debug.Log ("Pediente 1: " + pendiente1.ToString() + "Pediente 2: " + pendiente2.ToString());
+
+		float enemyWeight = 0f;
+		for (int i = 0; i < paraPesar.Count; ++i) {
+			enemyWeight += paraPesar [i];
+		}
+        aceleracion = (-(pendiente2 - pendiente1) * rotationSpeedWaves) // Player A
+			+ aceleracionPlayers * rotationSpeedPlayer // Waves A
+			+ enemyWeight * rotationEnemyCoefficient
+			- mRotationSpeed * rotationSpeedFriction;										// Friction A
 
 
-        mRotationSpeed += (aceleracion -mRotationSpeed*1f)*Time.deltaTime;
-        if (offsetY <= 0)
-            offsetY = 0.6f;
-        this.transform.position = new Vector3(transform.position.x, offsetY+0.3f, transform.position.z);
+        mRotationSpeed += aceleracion * Time.deltaTime;
+        //if (offsetY <= 0)
+        //    offsetY = 0.6f;
+        //this.transform.position = new Vector3(transform.position.x, offsetY+0.3f, transform.position.z);
 
         this.RotateShip(mRotationSpeed);
     }
 
-    public int addPeso(float toAdd)
+	public int addPeso(float toAdd, bool toRight)
     {
-        paraPesar.Add(toAdd);
+		if (toRight)
+        	paraPesar.Add(toAdd);
+		else
+			paraPesar.Add(-toAdd);
+        //Debug.Log(paraPesar.Count);
         return paraPesar.Count;
     }
 
@@ -123,4 +173,32 @@ public class CBarcoController : MonoBehaviour
         paraPesar.RemoveAt(idEvent);
     }
 
+	public void incrementRotationSpeed(float rotSpeed)
+	{
+		mRotationSpeed += rotSpeed;
+	}
+
+	void checkRotation()
+	{
+		if (Mathf.Abs(mTotalRotation) >= 90.0f) {
+			mEndGame = true;
+			StartCoroutine (EndGame());
+		}
+	}
+
+	IEnumerator EndGame()
+	{
+		yield return new WaitForSeconds (mEndDelay);
+		GameObject levelControllerObject = GameObject.FindGameObjectWithTag("GameController");
+		if(levelControllerObject)
+		{
+			CLevelController levelController = levelControllerObject.GetComponent<CLevelController>();
+			if (levelController) {
+			//	levelController.PedirGameOver ();
+				Debug.Log ("LevelController Game !!!!!");
+			}
+		}
+		
+		Debug.Log ("Eng Game !!!!!");
+	}
 }
---
+++ Assets/Scripts/EventsController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EventsController : MonoBehaviour {
+
+    public List<GameObject> eventos;
+    public float cdEvento;
+
+    private float temporizaor;
+    private bool jump;
+	void Start () {
+        temporizaor = 0;
+	}
+	
+	void Update () {
+        if(temporizaor>=cdEvento)
+            LlamarEventoRandom();
+        temporizaor += Time.deltaTime;
+	}
+
+    void LlamarEventoRandom()
+    {
+        int chosen = Random.Range(0,eventos.Count);
+        if (eventos[chosen].name.Equals("Seagull") ){ eventos[chosen].GetComponent<MoveSeagull>().siteToGo = GameObject.FindWithTag("Punto_1").transform; }
+        if (eventos[chosen].name.Equals("Seagull2")) { eventos[chosen].GetComponent<MoveSeagull>().siteToGo = GameObject.FindWithTag("Punto_2").transform; }
+        if (eventos[chosen].name.Equals("RotorPinguin")) {
+
+            GameObject myBrick = Instantiate(eventos[chosen]);
+            myBrick.transform.parent = GameObject.FindWithTag("SpawDelPinguino").transform;
+            jump = true;
+        }
+        if (!jump)
+        {
+            Instantiate(eventos[chosen]);
+        }
+        jump = false;
+        temporizaor = 0;
+    }
+    
+
+}
